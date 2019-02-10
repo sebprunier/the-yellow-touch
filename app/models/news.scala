da3582a44d1485055c59d9b32ca258d49cb0b66e
Sébastien Prunier <sebastien.prunier@gmail.com>
2019-02-10 23:36:13 +0100

    All news

--- app/models/news.scala
+++ app/models/news.scala
@@ -23,39 +23,44 @@ class NewsService @Inject()() {
           tags = Seq("Maxime Nicolle", "Asile Politique", "Corée du Nord")
         ),
         News(
-          title = "Maxime Nicolle quitte la France",
-          image = "maxime-nicolle-quitte-france.png",
-          tags = Seq("Maxime Nicolle", "Asile Politique", "Corée du Nord")
+          title = "Macron, Louis XIV, même destin",
+          image = "macron-louis-xiv.png",
+          tags = Seq("Macron", "Louis XIV", "Guillotine")
         ),
         News(
-          title = "Maxime Nicolle quitte la France",
-          image = "maxime-nicolle-quitte-france.png",
-          tags = Seq("Maxime Nicolle", "Asile Politique", "Corée du Nord")
+          title = "Les Gilets Jaunes sur PornHub",
+          image = "pornhub.png",
+          tags = Seq("Pornhub", "Tu l'as vu mon gilet ?")
         ),
         News(
-          title = "Maxime Nicolle quitte la France",
-          image = "maxime-nicolle-quitte-france.png",
-          tags = Seq("Maxime Nicolle", "Asile Politique", "Corée du Nord")
+          title = "Vous ne passerez pas !",
+          image = "you-shall-not-pass.png",
+          tags = Seq("Blocage", "Dédé", "On lâche rien")
         ),
         News(
-          title = "Maxime Nicolle quitte la France",
-          image = "maxime-nicolle-quitte-france.png",
-          tags = Seq("Maxime Nicolle", "Asile Politique", "Corée du Nord")
+          title = "Interview Konbini de Maxou",
+          image = "maxime-nicolle-konbini.png",
+          tags = Seq("Maxime Nicolle", "Konbini", "Tomber le masque")
         ),
         News(
-          title = "Maxime Nicolle quitte la France",
-          image = "maxime-nicolle-quitte-france.png",
-          tags = Seq("Maxime Nicolle", "Asile Politique", "Corée du Nord")
+          title = "Moi aussi je peux mettre un gilet",
+          image = "macron.png",
+          tags = Seq("Macron", "Je vous ai compris")
         ),
         News(
-          title = "Maxime Nicolle quitte la France",
-          image = "maxime-nicolle-quitte-france.png",
-          tags = Seq("Maxime Nicolle", "Asile Politique", "Corée du Nord")
+          title = "Le Jaune contre le Vert",
+          image = "ecolos-bobos.png",
+          tags = Seq("Ecolos", "Bobos", "Complots")
         ),
         News(
-          title = "Maxime Nicolle quitte la France",
-          image = "maxime-nicolle-quitte-france.png",
-          tags = Seq("Maxime Nicolle", "Asile Politique", "Corée du Nord")
+          title = "Pose les deux pieds en canard ♪",
+          image = "chenille.png",
+          tags = Seq("Chenille", "Rond-Point", "On lâche rien")
+        ),
+        News(
+          title = "Dédé, daltonien, soutient la cause",
+          image = "daltonien.png",
+          tags = Seq("Dédé", "CGT")
         )
       )
     )
